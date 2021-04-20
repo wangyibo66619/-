6d6e2a9156add55a2a4742b953626964ba6956be
wangyibo <75456485+wangyibo66619@users.noreply.github.com>
2021-04-20 19:14:30 +0800

    Add files via upload
    多态

---
+++ demo07/Person.java
@@ -0,0 +1,11 @@
+package oop.demo07;
+
+/**
+ * @author 王艺博
+ * @date 2021/4/19 13:18
+ */
+public class Person {
+    public void run() {
+        System.out.println("跑");
+    }
+}
---
+++ demo07/Student.java
@@ -0,0 +1,15 @@
+package oop.demo07;
+
+/**
+ * @author 王艺博
+ * @date 2021/4/19 13:18
+ */
+public class Student extends Person{
+    @Override
+    public void run() {
+        System.out.println("son");
+    }
+    public void eat() {
+        System.out.println("eat");
+    }
+}
